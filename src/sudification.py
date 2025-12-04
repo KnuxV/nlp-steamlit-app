96fc018cd9129590ad1bad179ff0f9fce47d1df4
SousMaring <lise.desire@sfr.fr>
2025-12-04 17:31:31 +0100

    Ajout gestion ponctuation

--- src/sudification.py
+++ src/sudification.py
@@ -3,9 +3,15 @@ def sudification(txt:str)-> str:
     listwords = txt.split()
     processedlist = []
     for word in listwords:
-        if re.match(r".*[aeiou]n$", word):
-            processedlist.append(word + "g")
+        match = re.match(r"(.*[aeiou]n)([.,!?]*)", word)
+        if match:
+            raw_word, punct = match.groups()
+            processedlist.append(raw_word + "g" + punct)
         else:
             processedlist.append(word)
     processedstring = ' '.join(processedlist)
     return processedstring
+
+
+
+
